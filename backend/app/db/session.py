51855eea0d6978f246e110ad48decf56f8bf618d
supersunho <supersunho@gmail.com>
2025-11-20 00:26:42 +0900

    update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -10,6 +10,8 @@ from app.core.config import settings
 from typing import AsyncGenerator
 import logging
 
+from app.db.base import Base
+
 logger = logging.getLogger(__name__)
 
 # Create async engine
